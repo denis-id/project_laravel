652aaac8b90c9fc05575a2c4c371c8e666e4dbf5
Anang Firmansyah <anangfirmansyahp5@gmail.com>
2024-12-20 16:04:19 +0800

    init

---
+++ resources/js/index.js
@@ -0,0 +1,9 @@
+import "../css/satoshi.css";
+import "../css/app.css";
+
+import Alpine from "alpinejs";
+import persist from "@alpinejs/persist";
+
+Alpine.plugin(persist);
+window.Alpine = Alpine;
+Alpine.start();
