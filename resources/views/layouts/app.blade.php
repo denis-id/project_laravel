79cce2585f809a1dc6eec73029bb04fb31faa0e9
Anang Firmansyah <anangfirmansyahp5@gmail.com>
2025-02-05 14:23:45 +0800

    fix: dashboard page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,13 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="csrf-token" content="{{ csrf_token() }}" />
   <title>
     eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template
   </title>
   @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/2.2.1/css/dataTables.tailwindcss.css">
 </head>
 
 <body
@@ -16,7 +19,7 @@
   x-init="
          darkMode = JSON.parse(localStorage.getItem('darkMode'));
          $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
-  :class="{'dark text-bodydark bg-boxdark-2': darkMode === true}">
+  :class="{'dark text-bodydark bg-boxdark-2': darkMode === true, 'bg-gray-100' : darkMode == false}">
   <!-- ===== Preloader Start ===== -->
   @include('partials.preloader')
   <!-- ===== Preloader End ===== -->
@@ -42,6 +45,41 @@
     <!-- ===== Content Area End ===== -->
   </div>
   <!-- ===== Page Wrapper End ===== -->
+
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+
+  @if (session('error'))
+  <script>
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: '{{ session('error') }}',
+    });
+  </script>
+  @endif
+
+  @if (session('success'))
+  <script>
+    Swal.fire({
+      icon: "success",
+      title: "Success",
+      text: '{{ session('success') }}'
+    })
+  </script>
+  @endif
+
+  <script>
+    $(document).ready(function() {
+      $.ajaxSetup({
+        headers: {
+          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+      });
+    })
+  </script>
+
+  @yield('scripts')
 </body>
 
 </html>
